1e7ec8d3a166c587f6305dc8f1c2b8fe72927aaf
zh_jun_xun <zjun1165@gmail.com>
2024-10-05 20:27:33 +0800

    测试

--- app/src/main/java/com/example/composetools/MainActivity.kt
+++ app/src/main/java/com/example/composetools/MainActivity.kt
@@ -26,6 +26,7 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.padding(innerPadding)
                     )
 
+
                 }
             }
         }
